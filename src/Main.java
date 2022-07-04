9bc9629e9adc6cd45450e03d0658b1ecea456a11
AaronsonRossita <aaronsonrossita@gmail.com>
2022-07-04 21:24:06 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+//       psvm
+    public static void main(String[] args){
+//       sout
+        System.out.println(5);
+
+        int y = 5;
+        int x;
+        x = 7;
+
+        int a = 2147483647;
+        System.out.println(8/0);
+    }
+}
+
+
+
+
+
+
+
