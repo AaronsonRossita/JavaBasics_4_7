c7b478f6779d6c84eb87ef2868ec59886f118203
AaronsonRossita <aaronsonrossita@gmail.com>
2022-07-11 21:19:58 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,104 @@
+import java.util.Scanner;
+
 public class Main {
 //       psvm
     public static void main(String[] args){
-//       sout
-        System.out.println(5);
+        Scanner scanner = new Scanner(System.in);
 
-        int y = 5;
-        int x;
-        x = 7;
+        int x = 5;
+        double y = 5.25;
 
-        int a = 2147483647;
-        System.out.println(8/0);
+        boolean b = true;
+
+        char c  = 'a';
+        String s = "hello world ! 45343 &(*&(#$#%";
+        String s1 = "HI";
+        // index =  0 1
+        //         'h' 'e' 'l
+        s1.charAt(0);
+//        System.out.println(s.charAt(1));
+//        System.out.println("print me");
+
+//        System.out.println(x);
+//        x = 8;
+//        System.out.println(x);
+//
+//        System.out.println("x = " + x);
+
+        int a = 5;
+        double a1 = 7;
+
+//        System.out.println(a > a1);
+
+//      array
+        int[] arrInt = {3,6,4,9};
+        double[] arrDouble = {};
+
+        String[] arrString = {"abc", "cde", "fgh"};
+
+//        System.out.println("arrString[1] = " + arrString[1] + "\narrString[2] = " + arrString[2]);
+
+
+
+//        fori
+        // 0 1 2 3
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println("my text");
+//        }
+//
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println("i = " + i);
+//        }
+//
+//        System.out.println("finish");
+
+        String[] myArray = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+//        System.out.println(myArray[0]);
+//        System.out.println(myArray[1]);
+//        System.out.println(myArray[2]);
+//        System.out.println(myArray[3]);
+//        System.out.println(myArray[4]);
+//        System.out.println(myArray[5]);
+//        System.out.println(myArray[6]);
+
+//        for (int i = 0; i < 7; i++) {
+//            System.out.println(myArray[i] + " & i at this moment is " + i);
+//            System.out.println();
+//        }
+//
+//        int i = 0;
+//        System.out.println(myArray[i]);
+//        i = 1;
+//        System.out.println(myArray[i]);
+//
+//        while (i >= 0){
+//            System.out.println("i'm stuck");
+//            i--;
+//        }
+
+//        String word1;
+//        word1 = myArray[0];
+//        System.out.println(word1);
+//        word1 = myArray[1];
+//        System.out.println(word1);
+//
+//        for (String yair : myArray){
+//            System.out.println(yair);
+//        }
+
+        String newWord = scanner.nextLine();
+        int age = scanner.nextInt();
+        System.out.println(age);
+
+
     }
+
+
+
 }
+
+
 
 
 
