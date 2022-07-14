82e5e284c04e987ad5d4784236b74e17f678a5da
AaronsonRossita <aaronsonrossita@gmail.com>
2022-07-14 21:19:13 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 //       psvm
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         int x = 5;
         double y = 5.25;
@@ -87,9 +89,68 @@ public class Main {
 //            System.out.println(yair);
 //        }
 
-        String newWord = scanner.nextLine();
-        int age = scanner.nextInt();
-        System.out.println(age);
+//        String newWord = scanner.nextLine();
+//        int age = scanner.nextInt();
+//        System.out.println(age);
+
+
+//        for (String day : myArray){
+//            System.out.println(day + 76545);
+//        }
+//
+//        System.out.println();
+//        System.out.println("the end");
+//
+//        int[] monthNumbers = {1,2,3,4,5,6,7,8,9,10,11,12,45,5,25,5,5,5,5,5,5,5,25,2,65,4758,74,2,24,21,5,54};
+//        int[] randomNumbers = {12,5,6,98,100};
+//
+//        for (int i = 0; i < randomNumbers.length; i++) {
+//            System.out.println(randomNumbers[i]);
+//        }
+
+
+//        for (int i = 5; i > 0; i--) {
+//            System.out.println(i);
+//        }
+
+
+
+        a = scanner.nextInt();
+
+        if (a > 3){
+            System.out.println("a is larger than 3");
+        }
+
+
+        if (a > 8){
+            System.out.println("a is bigger than 8");
+        }else{
+            System.out.println("a is smaller than 8");
+        }
+
+
+        if (a > 8){
+            System.out.println("a is bigger");
+        }else if (a == 8){
+            System.out.println("a is equal");
+        }else{
+            System.out.println("a is smaller");
+        }
+
+        System.out.println("i'm done");
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
